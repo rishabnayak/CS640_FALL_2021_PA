715c6a1888e0f40b865da42fa5a78c6e76c48505
Jun yue Feng <janefeng@ibm.com>
2021-09-27 17:23:44 -0400

    astar failed me

--- Controller.py
+++ Controller.py
@@ -4,6 +4,8 @@ from EnvironmentState import State
 from collections import deque
 import math
 import time
+import heapq
+from itertools import count
 
 LEFT = bytes.fromhex('00')
 UP = bytes.fromhex('01')
@@ -37,42 +39,81 @@ class Controller:
             ip, port)  # Initialize network manager and set environment dimensions
         self.state = State()  # Initialize empty state
         self.myInit()  # Initialize custom variables
+        self.dirArray = []
 
     # define your variables here
     def myInit(self):
         self.opMap = {LEFT: [-1, 0], RIGHT: [1, 0], UP: [0, -1], DOWN: [0, 1]}
         self.ops = list(self.opMap.keys())
 
-    def greedyAgent(self, head, food):
+    def addSnakeLocation(self):
+        snakeLocation = []
+        for part in self.state.body:
+            snakeLocation.append(str([part.x1, part.y1]))
+            snakeLocation.append(str([part.x2, part.y2]))
+            if part.x1 == part.x2:
+                for i in range(part.y1, part.y2):
+                    snakeLocation.append(str([part.x1, i]))
+            else:
+                for i in range(part.x1, part.x2):
+                    snakeLocation.append(str([i, part.y1]))
+        return snakeLocation
+
+    def aStar(self, head, food):
 
         # Don't move into any snakeLocation
-        # snakeLocation = []
-        # for part in self.state.body:
-        #     snakeLocation.append(tuple([part.x1, part.y1]))
-        #     snakeLocation.append(tuple([part.x2, part.y2]))
-        #     if part.x1 == part.x2:
-        #         for i in range(part.y1, part.y2):
-        #             snakeLocation.append(tuple([part.x1, i]))
-        #     else:
-        #         for i in range(part.x1, part.x2):
-        #             snakeLocation.append(tuple([i, part.y1]))
+        snakeLocation = self.addSnakeLocation()
 
         # Move to the closest food
         position = [head.x1, head.y1]
+        openList = []
+        visted = {str(position):True}
+        closeList = []
+        gScore = {}
+        hScore = {}
+        fScore = {}
+        tiebreaker = count()
+        self.dirArray = []
 
-        minDistance = math.inf
-        currentBestAction = None
+        heapq.heappush(openList, (0, -1, next(tiebreaker), position))
 
-        for i, action in enumerate(getStates(position, self.opMap)):
-            currentDistance = distance(action, food)
-            print(minDistance)
-            if not collisionDetection(action, self.state.body) and currentDistance < minDistance:
-                print(currentDistance)
-                minDistance = currentDistance
-                currentBestAction = i
+        # minDistance = math.inf
+        # currentBestAction = None
 
-        return self.ops[currentBestAction]
+        # for i, action in enumerate(getStates(position, self.opMap)):
+        #     currentDistance = distance(action, food)
+        #     print(minDistance)
+        #     if not collisionDetection(action, self.state.body) and currentDistance < minDistance:
+        #         print(currentDistance)
+        #         minDistance = currentDistance
+        #         currentBestAction = i
 
+        # return self.ops[currentBestAction]
+
+        #finds the next best path to the food base on current location
+        while(True):
+            temp = heapq.heappop(openList)
+            del visted[str(temp[3])]
+            closeList.append(str(temp[3]))
+            self.dirArray.append(temp[1])
+            if np.array_equal(temp[3], food):
+                break
+            for i, neighbor in enumerate(getStates(temp[3], self.opMap)):
+                if str(neighbor) not in closeList and str(neighbor) not in snakeLocation:
+                    if str(neighbor) not in gScore:
+                        tempg = 1
+                    else:
+                        tempg = gScore[str(neighbor)] + 1
+                    if str(neighbor) in visted:
+                        if tempg < gScore[str(neighbor)]:
+                            gScore[str(neighbor)] = tempg
+                    else:
+                        gScore[str(neighbor)] = tempg
+                    hScore[str(neighbor)] = distance(neighbor,food)
+                    fScore[str(neighbor)] = gScore[str(neighbor)] + hScore[str(neighbor)]
+                    heapq.heappush(openList, (fScore[str(neighbor)], i, next(tiebreaker), neighbor))
+                    visted[str(neighbor)] = True
+    
     def control(self):
         # Do not modify the order of operations.
         # Get current state, check exit condition and send next command.
@@ -82,9 +123,13 @@ class Controller:
             # 2. Check Exit condition
             if self.state.food == None:
                 break
+            #run astar to find the path 
+            if len(self.dirArray) == 0 or np.array_equal([self.state.body[0].x1,self.state.body[0].y1] , [self.state.food[0],self.state.food[1]] ):
+                self.aStar(self.state.body[0], self.state.food)
             # 3. Send next command
-            self.networkMgr.sendCommand(self.greedyAgent(
-                self.state.body[0], self.state.food))
+            # self.networkMgr.sendCommand(self.aStar(
+            #     self.state.body[0], self.state.food))
+            self.networkMgr.sendCommand(self.ops[self.dirArray.pop(0)])
 
 
 cntrl = Controller()
