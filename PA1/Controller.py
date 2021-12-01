c52f42b597e1fddb2be27d2c9a95113ea09d6b1f
Rishab Nayak <rishab@bu.edu>
2021-12-01 17:44:55 -0500

    pa4

---
+++ PA1/Controller.py
@@ -0,0 +1,112 @@
+import numpy as np
+from NetworkManager import NetworkManager
+from EnvironmentState import State
+from collections import deque
+import math
+import time
+import heapq
+from itertools import count
+
+LEFT = bytes.fromhex('00')
+UP = bytes.fromhex('01')
+RIGHT = bytes.fromhex('02')
+DOWN = bytes.fromhex('03')
+NOOP = bytes.fromhex('04')
+
+
+def distance(p1, p2):
+    return math.sqrt(np.sum(np.square(np.subtract(p1, p2))))
+
+
+def getStates(currentState, opMap):
+    head = np.array([currentState[0], currentState[1]])
+    return [head+opMap[op] for op in opMap.keys()]
+
+
+def collisionDetection(p1, body):
+    for line in body:
+        if distance(p1, [line.x1, line.y1]) + distance(p1, [line.x2, line.y2]) == distance([line.x2, line.y2], [line.x1, line.y1]):
+            return True
+    return False
+
+
+class Controller:
+
+    def __init__(self, ip='localhost', port=4668):
+        # Do not Modify
+        self.networkMgr = NetworkManager()
+        State.col_dim, State.row_dim = self.networkMgr.initiateConnection(
+            ip, port)  # Initialize network manager and set environment dimensions
+        self.state = State()  # Initialize empty state
+        self.myInit()  # Initialize custom variables
+    # define your variables here
+
+    def myInit(self):
+        self.opMap = {LEFT: [-1, 0], RIGHT: [1, 0], UP: [0, -1], DOWN: [0, 1]}
+        self.ops = list(self.opMap.keys())
+
+    def aStar(self, head, food):
+        position = [head.x1, head.y1]
+        openList = []
+        visited = {str(position): True}
+        closedList = []
+        gScore = {}
+        hScore = {}
+        fScore = {}
+        tiebreaker = count()
+        dirArray = []
+
+        heapq.heappush(openList, (0, -1, next(tiebreaker), position))
+
+        while(True):
+            temp = heapq.heappop(openList)
+            del visited[str(temp[3])]
+            closedList.append(str(temp[3]))
+            dirArray.append(temp[1])
+            if len(dirArray) > 1:
+                break
+            for i, neighbor in enumerate(getStates(temp[3], self.opMap)):
+                if str(neighbor) not in closedList and not collisionDetection(neighbor, self.state.body):
+                    if str(neighbor) not in gScore:
+                        tempg = 1
+                    else:
+                        tempg = gScore[str(neighbor)] + 1
+                    if str(neighbor) in visited:
+                        if tempg < gScore[str(neighbor)]:
+                            gScore[str(neighbor)] = tempg
+                        hScore[str(neighbor)] = distance(neighbor, food)
+                        fScore[str(neighbor)] = gScore[str(
+                            neighbor)] + hScore[str(neighbor)]
+                    else:
+                        gScore[str(neighbor)] = tempg
+                        hScore[str(neighbor)] = distance(neighbor, food)
+                        fScore[str(neighbor)] = gScore[str(
+                            neighbor)] + hScore[str(neighbor)]
+                        heapq.heappush(
+                            openList, (fScore[str(neighbor)], i, next(tiebreaker), neighbor))
+                        visited[str(neighbor)] = True
+            if np.array_equal(temp[3], food):
+                break
+        return self.ops[dirArray.pop(1)]
+
+    def control(self):
+        # Do not modify the order of operations.
+        # Get current state, check exit condition and send next command.
+        while(True):
+            # 1. Get current state information from the server
+            self.state.setState(self.networkMgr.getStateInfo())
+            # 2. Check Exit condition
+            if self.state.food == None:
+                break
+
+            # 3. Send next command
+            t1_start = time.perf_counter()
+            self.networkMgr.sendCommand(self.aStar(
+                self.state.body[0], self.state.food))
+            t1_stop = time.perf_counter()
+            print("Time per command:",
+                  t1_stop-t1_start)
+
+
+cntrl = Controller()
+cntrl.control()
--- PA4/Report.md
+++ PA4/Report.md
@@ -2,8 +2,18 @@
 
 ## Details about the pre-processing (cleaning) step you performed
 
-I preprocess the data to remove
+I preprocess the data to remove any tweets longer than the max tweet length, because that is bound to be garbage data. Since we're using bert-base-uncased, I convert all the tweets to lowercase, remove URL's and @mentions and replace them with spaces. I then remove nltk stopwords from the data to remove filler words and any words that don't necessarily add to the meaning of the tweet. This is followed by the removal of punctuation.
 
 ## A short description of the layers you added and your reasoning in addition to the training hyperparameters. ( Optimizer, Learning rate, batch size, Loss Function,...)
 
-## The performance of your models (BERT and Bayes Classifier) on the Test-CSV providedwith the dataset. For this, you can include a classification report. You are encouraged to use library functions to do this
+For the Naive Bayes portion of the assignment I chose to use the Complement Naive Bayes classifier since it's suited for imbalanced datasets. I tokenized the data using the RegexpTokenizer and vectorized the data using CountVectorizer.
+
+For BERT, I chose to use the bert-base-uncased model from TFHub, along with its associated tokenizer. I chose to use a batch size of 32 because that's what the authors of BERT recommended be used for retraining the model with different data. I used the Adam optimizer with a learning rate of 2e-5, again at the author's recommendations. I chose the Binary Crossentropy Loss function because the task is essentially binary classification.
+
+I appended a Batch Normalization layer to the pooled output from the BERT Classifier to normalize inputs for subsequent layers. This was followed by two sets of Dropout+FC Layers, and the final fully connected layer with a sigmoid activation function. The Dropout+FC layers improve the generalization performance of the model by randomly zeroing the inputs from a certain percentage of neurons making the features independent and not heavily correlated.
+
+## The performance of your models (BERT and Bayes Classifier) on the Test-CSV provided with the dataset. For this, you can include a classification report. You are encouraged to use library functions to do this
+
+The Bayes Classifier had a test accuracy of 82%. The precision and recall numbers were almost identical representing a well balanced dataset, and no overfitting.
+
+The BERT Classifier had a test accuracy of 84%. Again, the precision and recall numbers were close to each other, with the precision of a zero prediction slightly higher than that of that of a one, and the recall for one was slightly higher than that of zero. However, looking at the loss and accuracy plots, overfitting is apparent - loss keeps decreasing while validation loss keeps increasing, and validation accuracy stays the same. Changing the batch size, learning rate and dropout didn't do much to change this, which leads me to believe that the amount of data I'm training with - 0.01% of the training set (due to compute time limitations) was too small for the network to converge on, and with 100M parameters, the network tried to fit the data perfectly.
